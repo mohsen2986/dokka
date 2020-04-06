5aaf978e2ce5cf0e5b17a750b2c8d28f5c651e48
Andrzej Ratajczak <andrzej.ratajczak98@gmail.com>
2020-04-06 15:58:55 +0200

    Adds asynchronous rendering

--- core/src/main/kotlin/model/defaultValues.kt
+++ core/src/main/kotlin/model/defaultValues.kt
@@ -1,8 +1,6 @@
 package org.jetbrains.dokka.model
 
 import org.jetbrains.dokka.model.properties.ExtraProperty
-import org.jetbrains.dokka.model.properties.MergeStrategy
-import java.lang.IllegalStateException
 
 class DefaultValue(val value: String): ExtraProperty<DParameter> {
     companion object : ExtraProperty.Key<DParameter, DefaultValue>
